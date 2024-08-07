9500359d199857dbaffe6bb8f3d069b9d1ed29ca
rockydey077 <dev.rocky@jsencoder.com>
2024-08-07 10:58:58 +0600

    update live url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://moon-tech-project.netlify.app"],
   })
 );
 
